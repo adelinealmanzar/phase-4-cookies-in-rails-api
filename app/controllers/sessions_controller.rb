83f550aee7002dc1b6965f95cfcd003064834ccf
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-03-22 10:39:13 -0700

    commit tree a683293dc4a8d94db34565c163273e355b832040

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
   
   def index
-    session[:session_hello] ||= "World"
-    cookies[:cookies_hello] ||= "World"
+    session[:session_hello] ||= "Adeline was here"
+    cookies[:cookies_hello] ||= "Adeline was here"
     render json: { session: session, cookies: cookies.to_hash }
   end
 
